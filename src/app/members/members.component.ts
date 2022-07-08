7888584fe84e095643cb31645b8946022ae4b9b8
Julien CHARMIER <julien.charmier@biomerieux.com>
2022-07-08 10:05:52 +0200

    2 tables display OK

---
+++ src/app/members/members.component.ts
@@ -0,0 +1,75 @@
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {Member} from "../model/member";
+import {BackendService} from "../backend.service";
+import {Sort} from "@angular/material/sort";
+import {MatTable} from "@angular/material/table";
+import {Trip} from "../model/trip";
+
+@Component({
+  selector: 'app-members',
+  templateUrl: './members.component.html',
+  styleUrls: ['./members.component.css']
+})
+export class MembersComponent implements OnInit {
+  @ViewChild(MatTable) table!: MatTable<Trip>;
+  members: Member[] = [];
+  sortedMembers: Member[] = [];
+
+  constructor(private backendService: BackendService) { }
+
+  columnsToDisplay = ['name','tripsDriver','tripsPassenger', 'ratio', 'km','co2'];
+
+  ngOnInit(): void {
+    this.getMembers();
+    this.sortedMembers = this.members.slice();
+  }
+
+  getMembers(): void {
+    this.backendService.getMembers()
+      .subscribe(members => this.members = members);
+  }
+
+  getPassengerRatio(member: Member) : number{
+    return member.nbTripPassenger/member.nbTripDriver;
+  }
+
+  getKm(member: Member) : number {
+    return ((member.nbTripDriver+member.nbTripPassenger) * 46.3);
+  }
+
+  getCo2(member: Member) : number {
+    return (member.nbTripPassenger * 46.3) * 0.1;
+  }
+
+  sortData(sort: Sort) {
+    const data = this.members.slice();
+    if (!sort.active || sort.direction === '') {
+      this.sortedMembers = data;
+      return;
+    }
+
+    this.sortedMembers = data.sort((a, b) => {
+      const isAsc = sort.direction === 'asc';
+      switch (sort.active) {
+        case 'name':
+          return compare(a.name, b.name, isAsc);
+        case 'ratio':
+          return compare(a.nbTripPassenger/a.nbTripDriver, b.nbTripPassenger/b.nbTripDriver, isAsc);
+        case 'km':
+          return compare(a.nbTripPassenger+a.nbTripDriver, b.nbTripPassenger+b.nbTripDriver, isAsc);
+        case 'tripsDrivers':
+          return compare(a.nbTripDriver, b.nbTripDriver, isAsc);
+        case 'tripsPassenger':
+          return compare(a.nbTripPassenger, b.nbTripPassenger, isAsc);
+        default:
+          return 0;
+      }
+    });
+  }
+
+
+}
+
+function compare(a: number | string, b: number | string, isAsc: boolean) {
+  return (a < b ? -1 : 1) * (isAsc ? 1 : -1);
+}
