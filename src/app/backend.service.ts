44a658d8e0bd40eb81e756842a67fdc0ee24c43a
jul13nc <julien@charmier.net>
2022-07-13 16:18:47 +0200

    Add trajet add form

--- src/app/backend.service.ts
+++ src/app/backend.service.ts
@@ -1,11 +1,20 @@
 import { Injectable } from '@angular/core';
 import {Member} from "./model/member";
 import {Observable, of} from "rxjs";
+import {Trip} from "./model/trip";
+
+const EXAMPLE_DATA: Trip[] = [
+  {id: 1, date: new Date(Date.now()), driver: 'loeb', passengers:['helena']},
+
+];
 
 @Injectable({
   providedIn: 'root'
 })
 export class BackendService {
+
+// TODO: replace this with real data from your application
+
 
   constructor() { }
 
@@ -16,4 +25,9 @@ export class BackendService {
     ]);
     return heroes;
   }
+
+  getTrips(): Observable<Trip[]> {
+    return of(EXAMPLE_DATA)
+  }
+
 }
