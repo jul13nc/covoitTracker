db8ddaee1f81817f0517d657d8554a21f7caa667
Julien CHARMIER <julien.charmier@biomerieux.com>
2022-08-22 12:12:50 +0200

    Adding default sorting on Members table + review component to match Trips (explicit datasource, and no duplicate list of data)

--- src/app/members/members.component.ts
+++ src/app/members/members.component.ts
@@ -1,65 +1,52 @@
-import {Component, OnInit, ViewChild} from '@angular/core';
+import {AfterViewInit, ChangeDetectorRef, Component, OnInit, ViewChild} from '@angular/core';
 import {Member} from "../model/member";
 import {BackendService} from "../backend.service";
-import {Sort} from "@angular/material/sort";
+import {MatSort, Sort} from "@angular/material/sort";
 import {MatTable} from "@angular/material/table";
 import {Trip} from "../model/trip";
+import {MatPaginator} from "@angular/material/paginator";
+import {MembersDatasource} from "./members-datasource";
 
 @Component({
   selector: 'app-members',
   templateUrl: './members.component.html',
   styleUrls: ['./members.component.css']
 })
-export class MembersComponent implements OnInit {
-  @ViewChild(MatTable) table!: MatTable<Trip>;
-  members: Member[] = [];
-  sortedMembers: Member[] = [];
+export class MembersComponent implements AfterViewInit {
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+  @ViewChild(MatTable) table!: MatTable<Member>;
+  dataSource : MembersDatasource;
 
-  constructor(private backendService: BackendService) { }
+  constructor(private backendService: BackendService, private cdref: ChangeDetectorRef) {
+    this.dataSource = new MembersDatasource(backendService);
+  }
 
   columnsToDisplay = ['name','nbPoints', 'km','co2'];
 
-  ngOnInit(): void {
-    this.members = [];
-    this.getMembers();
-    this.sortedMembers = this.members.slice();
+  ngAfterViewInit(): void {
+    this.dataSource.sort = this.sort;
+    this.dataSource.paginator = this.paginator;
+    this.table.dataSource = this.dataSource;
+    this.sort.sort({disableClear: false, id:'nbPoints',start:'asc'});
+    this.cdref.detectChanges();
   }
 
-  getMembers(): void {
-    console.log("Getting members (current size is " + this.members.length)
-    this.backendService.getMembers()
-      .subscribe(members => {this.members = members
-      console.log("Received members " + members.length)});
-  }
+  //
+  // ngOnInit(): void {
+  //   this.members = [];
+  //   this.getMembers();
+  //   this.sortedMembers = this.members.slice();
+  //   // let defSort = {direction: 'asc', active:'nbPoints'} as Sort;
+  //   // this.sortData(defSort);
+  //   // this.sort.sort({disableClear: false, id:'nbPoints',start:'asc'});
+  // }
+  //
+  // getMembers(): void {
+  //   console.log("Getting members (current size is " + this.members.length)
+  //   this.backendService.getMembers()
+  //     .subscribe(members => {this.members = members
+  //     console.log("Received members " + members.length)});
+  // }
 
-  sortData(sort: Sort) {
-    const data = this.members.slice();
-    if (!sort.active || sort.direction === '') {
-      this.sortedMembers = data;
-      return;
-    }
-
-    this.sortedMembers = data.sort((a, b) => {
-      const isAsc = sort.direction === 'asc';
-      switch (sort.active) {
-        case 'name':
-          return compare(a.name, b.name, isAsc);
-        case 'nbPoints':
-          return compare(a.nbPoints, b.nbPoints, isAsc);
-        case 'km':
-          return compare(a.km, b.km, isAsc);
-        case 'co2':
-          return compare(a.co2, b.co2, isAsc);
-        // case 'tripsPassenger':
-        //   return compare(a.nbTripPassenger, b.nbTripPassenger, isAsc);
-        default:
-          return 0;
-      }
-    });
-  }
-
-}
-
-function compare(a: number | string, b: number | string, isAsc: boolean) {
-  return (a < b ? -1 : 1) * (isAsc ? 1 : -1);
 }
